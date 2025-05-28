0a0ca9e40c4c42015fc0e7724f211c98d431b1da
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2025-05-28 08:05:13 -0300

    feat: redirect to login page on home page load

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SEO from "@/components/SEO";
 import { HomePageTextsSEO } from "@/components/SEO/seoTexts";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const seoText = HomePageTextsSEO;
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/login");
+  }, [router]);
 
   return (
     <div>
