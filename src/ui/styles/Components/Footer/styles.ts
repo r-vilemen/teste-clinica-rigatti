41a83b9c763ee1314b9760a662a3953a20266d6a
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2025-05-28 11:26:26 -0300

    feat: add styled components for FooterWrapper and Strong elements

--- src/ui/styles/Components/Footer/styles.ts
+++ src/ui/styles/Components/Footer/styles.ts
@@ -1,3 +1,16 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const FooterWrapper = styled.footer`
+  text-align: center;
+  padding: 2rem 1rem;
+  font-size: 0.875rem;
+  color: ${({ theme }) => theme.colors.text.quaternary};
+  background-color: ${({ theme }) => theme.colors.backgrounds.tertiary};
+  border-top: 1px solid ${({ theme }) => theme.colors.border.colors.quaternary};
+  margin-top: 4rem;
+`;
+
+export const Strong = styled.span`
+  font-weight: 500;
+  color: ${({ theme }) => theme.colors.text.secondary};
+`;
